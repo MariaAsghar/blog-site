fda40289859053bc2d12f0e5c3e010f844ba945b
maryo180198@gmail.com <maryo180198@gmail.com>
2020-07-07 21:23:48 +0500

    First Commit

---
+++ src/build/precache-manifest.9d19a27566beac0f6a7fda213875158a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fa266a105646f0f437613d40ecbb21c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90e57272558957ff3dd3",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "b7f97570d2195be55753",
+    "url": "/static/js/2.04a3ce0a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.04a3ce0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90e57272558957ff3dd3",
+    "url": "/static/js/main.e6f23935.chunk.js"
+  },
+  {
+    "revision": "c0c6fa7a992c8ddc1857",
+    "url": "/static/js/runtime-main.e0354eb9.js"
+  }
+]);
